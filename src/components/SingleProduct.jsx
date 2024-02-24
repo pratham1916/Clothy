7dc30790222f168eaef0f67cf7cf78a6d068a5c5
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-02-24 21:51:11 +0530

    singleProductPage

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,12 +1,17 @@
-
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { Button } from '@chakra-ui/react';
+import StarRating from "./StarRating";
+import "../style/singleProduct.css"
+
 function SingleProduct() {
   const [user, setUser] = useState({});
+  const [quantity, setQuantity] = useState(1);
 
   const params = useParams();
   const location = useLocation();
+
   useEffect(() => {
     axios
       .get(`https://clothy-api.onrender.com/womens/${params.id}`)
@@ -15,14 +20,55 @@ function SingleProduct() {
         console.log("error here");
       });
   }, [params.id]);
+
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity((prevQuantity) => prevQuantity - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+
+    let obj = {
+
+      quantity: quantity,
+    };
+
+  };
+
   return (
     <div>
-      <p>{params.id}</p>
-      <p>{location.state}</p>
-      <p>{location.pathname}</p>
-      {JSON.stringify(user)}
+      <div className="singleproductcontainer">
+        <img src={user.imageURL} alt={user.name} />
+        <div className="singleInfo">
+          <h4>{user.name}</h4>
+          <h5>{user.description}</h5>
+          <p className="product-detail"><b>Price:</b> ${user.price}</p>
+          <p className="product-detail"><StarRating rating={user.rating} /></p>
+          <div className="single_button">
+
+            <div className="quantity-container">
+              <button className="quantity-button" onClick={handleDecrement}>-</button>
+              <span className="quantity">{quantity}</span>
+              <button className="quantity-button" onClick={handleIncrement}>+</button>
+            </div>
+            <Button colorScheme='red' size='md' onClick={handleAddToCart}>
+              ADD TO CART
+            </Button>
+          </div>
+          <p className="product-para">
+            Explore the details of this amazing product. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SingleProduct
+export default SingleProduct;
