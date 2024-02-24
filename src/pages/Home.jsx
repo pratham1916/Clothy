6ab4057b4ba15fb912f39ee260b2c3e57df1d2fd
pratham1916 <120631770+pratham1916@users.noreply.github.com>
2024-02-24 15:38:40 +0530

    Styling For Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,33 +39,28 @@ const Home = () => {
   };
 
   return (
-
-    <div>
-
-      <div id="cnv">
-        <div id="cnv1"><img src={cnv1} alt="cnv1" /></div>
-        <div id="cnv2"><h1>CLOTHS ARE THE SPIRIT OF FASHION</h1></div>
-        <div id="cnv3"><img src={cnv3} alt="cnv3" /></div>
+    <div id="home">
+      <div class="flex-container">
+        <img src={cnv1} alt="cnv1" />
+        <h1>CLOTHS ARE THE SPIRIT OF FASHION</h1>
+        <img src={cnv3} alt="cnv3" />
       </div>
-
-
       <div id="newbx">
-        <div id="newbx1"><img src={logo1} alt="newbx1" /></div>
-        <div id="newbx2"><img src={lgo} alt="newbx2" /></div>
-        <div id="newbx3"><img src={logo2} alt="newbx3" /></div>
+        <img src={logo1} alt="newbx1" />
+        <img src={lgo} alt="newbx2" />
+        <img src={logo2} alt="newbx3" />
       </div>
-
 
       <div className='text-container'>
-        <div id='tex1'>
-          <h1 id='hea1'>WE THINK YOU'LL <span id='spa1'>LIKE THESE</span></h1>
+        <div id='text1'>
+          <h1 id='header1'>WE THINK YOU'LL <span id='span1'>LIKE THESE</span></h1>
           <p>Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories. The latest fashion news, beauty coverage, celebrity style.</p>
-          <div id='tx2'>
-            <div id='tx21'>
+          <div id='text2'>
+            <div id='text21'>
               <h5>Care instructions</h5>
               <p>Machine wash at 30°C</p>
             </div>
-            <div id='tx23'>
+            <div id='text23'>
               <h5>Fabric material</h5>
               <p>84% cotton, 16% polyester</p>
             </div>
@@ -74,46 +69,57 @@ const Home = () => {
         <img src={p1c2} alt="img6" />
       </div>
 
-      <div id="box">
-
-        <div id='bx'>
-          <img id='i2' src={arr} alt="" /><img id='i1' src={c2} alt="" /></div>
-        <div id="box1">
-          <div id='box3'><h4>Pattern</h4> <p>Colourful & Print</p></div>
-          <div id='box4'><h4>We are the online platform platform for fashion and lifestyle. </h4></div>
-          <div id='box5'><p id='px'>We are groen from a pioneer in e-commerce to become a leading Europian online platfrom.
-          </p>
-            <p>Read more --</p></div>
-          <div id='box6'><h4>Material</h4> <p>100% cotton & polyester</p></div>
-        </div>
-
-      </div>
-      <h3 id='newarrivals'>New Arrivals </h3>
-      <div className="App">
+      <div id="crousel">
+        <h3><span id='span1'>New Arrivals</span> </h3>
         <Carousel {...carouselOptions}>
           {product}
         </Carousel>
-        <div className="inspiration-container">
-          <div className="inspiration-text">
-            <h2>Outfit Inspiration: Snag Their Style</h2>
-            <p>Open Communication and participation are pivotal elements of our company culture. How a bold idea Spawned a leading European Online platform.</p>
-          </div>
-          <div className="inspiration-visual">
-            <img className="style-image" src={shopping} alt="Shopping" />
-          </div>
+      </div>
+
+      <div id="box">
+        <div id='bx'><img class='img-circle-large' src={c2} alt="" /></div>
+        <div id="box1">
+          <div class='box-detail'><h4>Pattern</h4> <p>Colourful & Print</p></div>
+          <div class='box-content'><h4>We are the online platform platform for fashion and lifestyle. </h4></div>
+          <div class='box-content'><p class='content-paragraph'>We are groen from a pioneer in e-commerce to become a leading European online platform.
+          </p>
+            <p>Read more --</p></div>
+          <div class='box-detail'><h4>Material</h4> <p>100% cotton & polyester</p></div>
         </div>
       </div>
 
-      <div id="head">
-        <p>FOLLOW US ON</p>
-        <h4><i class="fa-brands fa-instagram"></i> @clothy.fashion</h4>
+
+      <div id="crousel">
+        <h3><span id='span1'>New Arrivals</span> </h3>
+        <Carousel {...carouselOptions}>
+          {product}
+        </Carousel>
       </div>
-      <div id="card-container">
-        <div id='card1' className="card"><img src={c1} alt="img1" /></div>
-        <div className="card"><img src={c2} alt="img2" /></div>
-        <div className="card"><img src={c3} alt="img3" /></div>
-        <div className="card"><img src={c4} alt="img4" /></div>
-        <div id='card1' className="card"><img src={c5} alt="img5" /></div>
+
+
+      <div className="inspiration-container">
+        <div className="inspiration-text">
+          <h2>Outfit Inspiration: Snag Their Style</h2>
+          <p>Open Communication and participation are pivotal elements of our company culture. How a bold idea Spawned a leading European Online platform.</p>
+        </div>
+        <div className="inspiration-visual">
+          <img className="style-image" src={shopping} alt="Shopping" />
+        </div>
+      </div>
+
+
+      <div className="cards">
+        <div id="head">
+          <p>FOLLOW US ON</p>
+          <h4><i class="fa-brands fa-instagram"></i> @clothy.fashion</h4>
+        </div>
+        <div id="card-container">
+          <div className="card"><img src="https://images.herzindagi.info/image/2022/Jan/shopping-tips-for-save-money.jpg" alt="img1" /></div>
+          <div className="card"><img src={c2} alt="img2" /></div>
+          <div className="card"><img src={c3} alt="img3" /></div>
+          <div className="card"><img src={c4} alt="img4" /></div>
+          <div className="card"><img src="https://img.freepik.com/free-photo/studio-shot-pretty-black-woman-with-white-shopping-bag-standing-yellow-background-trendy-spring-fashionable-look_273443-10.jpg" alt="img5" /></div>
+        </div>
       </div>
 
 
