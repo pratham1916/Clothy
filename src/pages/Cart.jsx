954fbdd0becd2a5d2305c6cffe35f5b26c45b344
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-02-24 21:58:03 +0530

    .

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,16 +2,16 @@ import React, { useEffect } from 'react'
 
 import { useSelector } from 'react-redux';
 const Cart = () => {
-    let user = JSON.parse(localStorage.getItem("Users"))||[];
-     const{isAuth} = useSelector(state=>state.login);
-     console.log(isAuth,"line 7")
-       useEffect(()=>{
-           
-       },[]);
-    
+  let user = JSON.parse(localStorage.getItem("Users")) || [];
+  const { isAuth } = useSelector(state => state.login);
+  console.log(isAuth, "line 7")
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div>
-      
+
     </div>
   )
 }
